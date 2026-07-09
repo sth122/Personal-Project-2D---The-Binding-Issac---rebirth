651a52a9af3c3db22b78473b221b0237f5acabc2
sth122 <132740922+sth122@users.noreply.github.com>
2026-07-09 16:33:28 +0900

    Firts commit
    프로젝트 파일 첫 커밋

---
+++ Assets/01.Scripts/Manager/Singleton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private bool dontDestroy = true;
+    private static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = InitManager<T>();
+            }
+            return instance;
+        }
+    }
+
+    protected static U InitManager<U>() where U : MonoBehaviour
+    {
+        GameObject go = null;
+        U obj = FindAnyObjectByType<U>();
+        if(obj == null)
+        {
+            go = new GameObject(typeof(U).Name);
+            go.AddComponent<U>();
+        }
+        else
+        {
+            go = obj.gameObject;
+        }
+
+        return obj.GetComponent<U>();
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            if(dontDestroy)
+            {
+                Instance.Init();
+            }
+            else
+            {
+                instance = GetComponent<T>();
+                Init();
+            }
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    protected abstract void Init();
+}
---
+++ Assets/01.Scripts/StateMachine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public interface IState
+{
+    public void Enter();
+    public void Exit();
+    public void Update();
+    public void FixedUpdate();
+}
+
+public abstract class StateMachine
+{
+    protected IState currentState;
+    public void ChangeState(IState state)
+    {
+        currentState.Exit();
+        currentState = state;
+        currentState.Enter();
+    }
+
+    public void Update() => currentState?.Update();
+
+}
