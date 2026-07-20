2e07c3f769ff02494f2c5f58d20ef48ef50cc66d
sth122 <132740922+sth122@users.noreply.github.com>
2026-07-20 14:48:48 +0900

    commit
    카메라 위치 이동 구현 중

---
+++ Assets/01.Scripts/CameraRoomRock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraRoomRock : Singleton<CameraRoomRock>
+{
+    
+    #region Variable
+    [SerializeField] Transform target;
+    Camera camera;
+    Coroutine cameraShake;
+    Vector3 targetPos;
+    Vector3 velocity;
+    Vector3 originPos;
+    #endregion
+
+    private void Start()
+    {
+        camera = Camera.main;
+        velocity = Vector3.zero;
+    }
+
+    private void LateUpdate()
+    {
+        targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+    public void CameraStop()
+    {
+        if (cameraShake != null)
+        {
+            StopCoroutine(cameraShake);
+        }
+        transform.position = originPos;
+    }
+}
