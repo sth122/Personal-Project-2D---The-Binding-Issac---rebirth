08c193043fdd1d8232a40b2935e885b426cd3ac1
sth122 <132740922+sth122@users.noreply.github.com>
2026-07-30 15:36:48 +0900

    Mainmenu Page_Title

--- Assets/01.Scripts/02.Manager/UIManager.cs
+++ Assets/01.Scripts/02.Manager/UIManager.cs
@@ -2,5 +2,6 @@
 
 public class UIManager : Singleton<UIManager>
 {
-    
+
+
 }
---
+++ Assets/01.Scripts/03.UI/MainMenu/MainMenuController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Scroll Settings")]
+    public RectTransform menuContainer;
+    public float smoothTime = 1f;
+    public float pageHeight = 1000f;
+    private float targetPositionY = 0f;
+    public bool isAtTilte = true;
+
+    private float currentVelocity = 1f;
+
+    private void Update()
+    {
+        HandleScrolling();
+
+
+        if (isAtTilte)
+        {
+            CheckInpuForNextPage();
+        }
+    }
+
+    private void HandleScrolling()
+    {
+        if (menuContainer != null)
+        {
+            Vector2 currentPos = menuContainer.anchoredPosition;
+            float newY = Mathf.SmoothDamp(currentPos.y, targetPositionY, ref currentVelocity, smoothTime);
+            menuContainer.anchoredPosition = new Vector2(currentPos.x, newY);
+        }
+    }
+
+    private void CheckInpuForNextPage()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame || Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            GotoNextPage();
+        }
+    }
+    public void GotoNextPage()
+    {
+        isAtTilte = false;
+        targetPositionY = pageHeight;
+    }
+}
