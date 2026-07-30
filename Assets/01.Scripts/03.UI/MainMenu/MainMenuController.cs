e5c4188f13fee8430a0562ee8973b857cc56599a
sth122 <132740922+sth122@users.noreply.github.com>
2026-07-30 16:47:01 +0900

    UI InputSystem

--- Assets/01.Scripts/03.UI/MainMenu/MainMenuController.cs
+++ Assets/01.Scripts/03.UI/MainMenu/MainMenuController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
@@ -9,19 +12,32 @@ public class MainMenuController : MonoBehaviour
     public float smoothTime = 1f;
     public float pageHeight = 1000f;
     private float targetPositionY = 0f;
-    public bool isAtTilte = true;
+    private bool isAtTilte = true;
+    private bool isAtFileSelect = false;
+
+    private IsaacInputActions inputAction;
 
     private float currentVelocity = 1f;
+
+    private void Awake()
+    {
+        inputAction = new IsaacInputActions();
+        inputAction.UI.Submit.performed += _ => OnSubmitPressed();
+        inputAction.UI.Cancel.performed += _ => OnCancelPressed();
+    }
+
+    private void OnEnable()
+    {
+        inputAction.Enable();
+    }
+    private void OnDisable()
+    {
+        inputAction.Disable();
+    }
 
     private void Update()
     {
         HandleScrolling();
-
-
-        if (isAtTilte)
-        {
-            CheckInpuForNextPage();
-        }
     }
 
     private void HandleScrolling()
@@ -34,16 +50,57 @@ public class MainMenuController : MonoBehaviour
         }
     }
 
-    private void CheckInpuForNextPage()
-    {
-        if(Keyboard.current.spaceKey.wasPressedThisFrame || Keyboard.current.enterKey.wasPressedThisFrame)
-        {
-            GotoNextPage();
-        }
-    }
     public void GotoNextPage()
     {
-        isAtTilte = false;
-        targetPositionY = pageHeight;
+        targetPositionY += pageHeight;
+        CheckTitle();
     }
+
+    public void GoToPreviousPage()
+    {
+        targetPositionY -= pageHeight;
+        CheckTitle();
+    }
+
+    private void CheckTitle()
+    {
+        if (targetPositionY == 0f)
+        {
+            isAtTilte = true;
+            isAtFileSelect = false;
+        }
+        else if (targetPositionY == pageHeight)
+        {
+            isAtTilte = false;
+            isAtFileSelect = true;
+        }
+    }
+
+    private void OnSubmitPressed()
+    {
+        StartCoroutine(CheckRoutine(CheckTitle, GotoNextPage));
+    }
+    private void OnCancelPressed()
+    {
+        StartCoroutine(CheckRoutine(CheckTitle,
+            () =>
+            {
+                if (isAtTilte) QuitGame();
+                else GoToPreviousPage();
+            }));
+    }
+
+    private IEnumerator CheckRoutine(Action onCheck, Action onNext)
+    {
+        onCheck?.Invoke();
+        yield return null;
+        onNext?.Invoke();
+    }
+    private void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.LogError("게임 종료");
+    }
+
 }
